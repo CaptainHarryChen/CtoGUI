f54f207674d30e7be3394db6e1914be5b7218da8
CaptainHarryChen <2429643516@qq.com>
2017-07-05 11:46:21 +0800

    建好工程结构

---
+++ ctogui.h
@@ -0,0 +1,22 @@
+#ifndef __CTOGUI_H__
+#define __CTOGUI_H__
+
+#include"main.h"
+#include<GL/glut.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+class DLL_EXPORT CtoGui
+{
+
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __CTOGUI_H__
