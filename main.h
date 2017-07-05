6b8ef82bbe98a33c258e54b615dac1959b1ca00b
CaptainHarryChen <captainharrychen@qq.com>
2017-07-05 11:46:21 +0800

    建好工程结构

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
