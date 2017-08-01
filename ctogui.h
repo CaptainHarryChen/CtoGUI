fc0cf36df26649c130e70d780102886ee01338a5
CaptainHarryChen <captainharrychen@qq.com>
2017-08-01 18:11:25 +0800

    坐标优化

--- ctogui.h
+++ ctogui.h
@@ -14,10 +14,10 @@ extern "C"
 
 namespace CtoGui
 {
-    void DLL_EXPORT Init(int *argc, char **argv, int width, int height, const char *title);
-    void DLL_EXPORT SetImgSize(int iw, int ih);
+    void DLL_EXPORT Init(int *argc, char **argv, int height, int width, const char *title);
+    void DLL_EXPORT SetImgSize(int ih, int iw);
     void DLL_EXPORT SetImg(char ch, const char *bpath);
-    void DLL_EXPORT SetScreenSize(int iw, int ih);
+    void DLL_EXPORT SetScreenSize(int ih, int iw);
     void DLL_EXPORT SetUpdateTimer(int tim);
     void DLL_EXPORT SetUpdateScreenFunc(void(*p)());
     void DLL_EXPORT SetKeyFunc(void(*p)(unsigned char, int, int));
