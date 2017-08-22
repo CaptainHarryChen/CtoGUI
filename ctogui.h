31be653f569b498a27c72d7ae937a3b109b655de
CaptainHarryChen <captainharrychen@qq.com>
2017-08-22 18:44:46 +0800

    更新dll头文件

--- ctogui.h
+++ ctogui.h
@@ -1,7 +1,24 @@
 #ifndef __CTOGUI_H__
 #define __CTOGUI_H__
 
-#include"main.h"
+#include <windows.h>
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#endif // BUILD_DLL
+#ifdef TEST_DLL
+    #define DLL_EXPORT
+#endif // TEST_DLL
+#ifndef BUILD_DLL
+    #ifndef TEST_DLL
+        #define DLL_EXPORT __declspec(dllimport)
+    #endif // TEST_DLL
+#endif // BUILD_DLL
+
 #include<GL/glut.h>
 #include<GL/glext.h>
 
